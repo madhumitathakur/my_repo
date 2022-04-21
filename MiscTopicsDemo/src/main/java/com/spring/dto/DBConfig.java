e884b1530eaa4ddea5d5d58ef4ad41b1c660e831
MK66216 <MK66216@HRW42NLPC0547.ind.zensar.com>
2022-04-21 12:39:34 +0530

    first commit

---
+++ MiscTopicsDemo/src/main/java/com/spring/dto/DBConfig.java
@@ -0,0 +1,13 @@
+package com.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DBConfig {
+	private String url;
+	private String username;
+	
+	
+}
